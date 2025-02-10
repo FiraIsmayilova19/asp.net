95483144a06b7df0420b314d86690b73a44beee0
FiraIsmayilova19 <ismayilovafira004@gmail.com>
2025-02-11 01:55:09 +0400

    IdentityTask

---
+++ ECommerceAppIdentity/ECommerceApp_Afternoon/ECommerceApp.UI/Controllers/CartController.cs
@@ -0,0 +1,102 @@
+﻿using ECommerce.Entities.Concrete;
+using ECommerceApp.Business.Abstract;
+using ECommerceApp.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.UI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartSessionService _cartSessionService;
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+
+        public CartController(ICartSessionService cartSessionService, ICartService cartService, IProductService productService)
+        {
+            _cartSessionService = cartSessionService;
+            _cartService = cartService;
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> AddToCart(int productId, int page, int category)
+        {
+            var productToBeAdded = await _productService.GetByIdAsync(productId);
+            var cart = _cartSessionService.GetCart();
+
+            _cartService.AddToCart(cart, productToBeAdded);
+            _cartSessionService.SetCart(cart);
+
+            TempData.Add("message", $"Your Product , {productToBeAdded.ProductName} was added successfully.");
+
+            return RedirectToAction("Index", "Product", new { page = page, category = category });
+        }
+
+        public IActionResult List()
+        {
+            var cart = _cartSessionService.GetCart();
+            var model = new CartListViewModel
+            {
+                Cart = cart
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Complete()
+        {
+            var shippingDetailViewModel = new ShippingDetailsViewModel
+            {
+                ShippingDetails=new ShippingDetails()
+            };
+            return View(shippingDetailViewModel);   
+        }
+
+        [HttpPost]
+        public IActionResult Complete(ShippingDetailsViewModel model)
+        {
+            if(!ModelState.IsValid) {
+                return View(model);
+            }
+            TempData.Add("message", $"You {model.ShippingDetails.Firstname} your order is in progress.");
+            return RedirectToAction("List");
+        }
+        public IActionResult Remove(int productId)
+        {
+            var cart = _cartSessionService.GetCart();
+            _cartService.RemoveFromCart(cart, productId);
+            _cartSessionService.SetCart(cart);
+            return RedirectToAction("List");
+        }
+        public IActionResult Plus(int productId)
+        {
+            var cart = _cartSessionService.GetCart();
+            var product = _productService.GetByIdAsync(productId).Result;
+            _cartService.AddToCart(cart, product);
+            _cartSessionService.SetCart(cart);
+            return RedirectToAction("List");
+        }
+        public IActionResult Minus(int productId)
+        {
+            var cart = _cartSessionService.GetCart();
+            var product = _productService.GetByIdAsync(productId).Result;
+            foreach (var item in cart.CartLines)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    if (item.Quantity > 1)
+                    {
+                        item.Quantity -= 1;
+                    }
+                    else if (item.Quantity == 1)
+                    {
+                        return Remove(productId);
+                    }
+                }
+            }
+            _cartSessionService.SetCart(cart);
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
