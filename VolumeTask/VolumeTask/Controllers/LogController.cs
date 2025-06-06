c3e05911460942ebbe9ad40f3c2ccad541810a42
FiraIsmayilova19 <ismayilovafira004@gmail.com>
2025-06-06 16:48:38 +0400

    volume

---
+++ VolumeTask/VolumeTask/Controllers/LogController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VolumeTest1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly string logPath = "/Logs/logs.txt";
+
+        [HttpPost("info")]
+        public IActionResult LogInfo([FromBody] string message)
+        {
+            WriteLog("INFO", message);
+            return Ok("Info log yazıldı.");
+        }
+
+        [HttpPost("error")]
+        public IActionResult LogError([FromBody] string message)
+        {
+            WriteLog("ERROR", message);
+            return Ok("Error log yazıldı.");
+        }
+
+        private void WriteLog(string level, string message)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+
+            using (var stream = new FileStream(logPath, FileMode.Append, FileAccess.Write, FileShare.Read))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {message}");
+            }
+        }
+    }
+}
